a8366c14cc2dcdce3a744e4c5869c5b8472f149e
vitimerida <81519967+vitimerida@users.noreply.github.com>
2021-09-29 23:05:12 +0200

    Update and rename nomeva.py to web_scrapper.py

--- web_scrapper.py
+++ web_scrapper.py
@@ -30,20 +30,24 @@ soup = get_data(url)
 link_list = parse(soup)
 for i in link_list:
     r = requests.get(i)
-    soup = BeautifulSoup(r.text,"html.parser")
+    soup2 = BeautifulSoup(r.text,"html.parser")
     print("copy one seccion")
     try:
         productlist = []
-        results = soup.find_all("div",{"class":"product-list__item"})
+        results = soup2.find_all("div",{"class":"product-list__item"})
         for item in results:
             product = {
                 "title": item.find("span", {"class": "details"}).text,
-                "price": float(item.find("p", {"class":"price"}).text.replace("&nbsp€","").strip().split()[0]),
-                "link" : item.find("a").get("href").text,
+                "price": (item.find("p", {"class":"price"}).text.replace("&nbsp€","").strip().split()[0]),
+                #"link" : item.find("a").get("href").text, Can´t run the programm wtih this sentence
             }
             productlist.append(product)
         listdf = pd.DataFrame(productlist)
+        print(listdf)
         listdf.to_csv("hello.csv" ,mode="a",header=False)
+        read_file = pd.read_csv("hello.csv")
+        read_file.to_excel("hello.xlsx", index=None)
+
 
     except:
         print("the recollection of data can´t be possible")
